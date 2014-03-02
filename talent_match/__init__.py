f5d2c6d30cd72ae2085877551abe327c585c1310
Jordan <jmwright798@gmail.com>
2014-03-01 23:38:07 -0600

    Fixed /register endpoints on index page Added login_view endpoint Added appropriate login required flash message category

--- talent_match/__init__.py
+++ talent_match/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 bcrypt = Bcrypt(app)
 
 from talent_match import views, models, forms
