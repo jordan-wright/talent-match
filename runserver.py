5b63f0d4d0871e88a5de3ceb95fd32150d48de36
S. Mazzucco <steve_1373@yahoo.com>
2014-03-02 18:30:19 -0600

    Checking db-related changes and the early start of a category/skill view.

--- runserver.py
+++ runserver.py
@@ -3,8 +3,7 @@ from config import basedir
 import os.path
 
 # Create the db if it doesn't exist
-if not os.path.exists(os.path.join(basedir, 'talent_match.db')):
+if not os.path.exists(os.path.join(basedir, 'talent-match.db')):
 	print "Creating database..."
 	db.create_all()
-
 app.run(debug=True)
