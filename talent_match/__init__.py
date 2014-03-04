3b7e6cc7ac5a36b0093ef966a778f06f4b58e992
S. Mazzucco <steve_1373@yahoo.com>
2014-03-04 15:35:38 -0600

    Moved the addTestData out of the models.py into a startup module.

--- talent_match/__init__.py
+++ talent_match/__init__.py
@@ -12,10 +12,12 @@ login_manager.login_message_category = 'info'
 bcrypt = Bcrypt(app)
 
 from talent_match import views, models, forms
+from talent_match.startup import addTestData
 
 def createTestData():
     models.addTestData()
-app.createTestData = createTestData
+#app.createTestData = createTestData
+app.createTestData = addTestData
 
 # Provide the user loader to the login manager
 @login_manager.user_loader
