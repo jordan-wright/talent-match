09d306ff8dd120338aca340804ed93d41aabe9bf
Jordan <jmwright798@gmail.com>
2014-04-02 21:23:13 -0500

    Commented out import statement

--- talent_match/views.py
+++ talent_match/views.py
@@ -2,7 +2,7 @@ from talent_match import app, db, bcrypt
 from flask import render_template, request, redirect, url_for, flash, g, jsonify
 from flask.ext.login import login_user, login_required, logout_user
 from models import User, Category, Skill, Seeker, Provider, ProviderSkill, Activity, ActivitySkill
-from forms import LoginForm, RegisterForm, EditProfileForm, EditCategoryForm, EditSkillForm, SearchForm, ActivityForm
+from forms import LoginForm, RegisterForm, EditProfileForm, EditCategoryForm, EditSkillForm, SearchForm#, ActivityForm
 from sqlalchemy.sql import func
 from functools import wraps
 from config import POSTS_PER_PAGE
